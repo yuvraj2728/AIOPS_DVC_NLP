9d122cf74b4ed5f2c3ed48f5e867542b2aadf819
yuvaraj2728 <yuvarajs2728@gmail.com>
2022-07-12 20:44:24 +0530

    New project without google drive

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -8,7 +8,7 @@ from src.utils.data_mgmt import process_posts
 import random
 
 
-STAGE = "stage 02 prepare data" ## <<< change stage name 
+STAGE = "One"
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -17,15 +17,13 @@ logging.basicConfig(
     filemode="a"
     )
 
-
 def main(config_path, params_path):
-    ## read config files
+    ## converting XML data tsv
     config = read_yaml(config_path)
     params = read_yaml(params_path)
 
-    local_data_dir = config["source_download_dirs"]["data_dir"]
-    data_filename = config["source_download_dirs"]["data_file"]
-    input_data = os.path.join(local_data_dir, data_filename) 
+    source_data = config["source_data"]
+    input_data = os.path.join(source_data["data_dir"], source_data["data_file"])
 
     split = params["prepare"]["split"]
     seed = params["prepare"]["seed"]
@@ -38,12 +36,13 @@ def main(config_path, params_path):
 
     train_data_path = os.path.join(prepared_data_dir_path, artifacts["TRAIN_DATA"])
     test_data_path = os.path.join(prepared_data_dir_path, artifacts["TEST_DATA"])
-
+    
     encode = "utf8"
-    with open(input_data, encoding=encode) as f_in:
-        with open(train_data_path, "w", encoding=encode) as f_out_train:
-            with open(test_data_path, "w", encoding=encode) as f_out_test:
-                process_posts(f_in, f_out_train, f_out_test, "<python>", split)
+    with open(input_data, encoding=encode) as fd_in:
+        with open(train_data_path, "w", encoding=encode) as fd_out_train:
+            with open(test_data_path, "w", encoding=encode) as fd_out_test:
+                process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
+
 
 
 
